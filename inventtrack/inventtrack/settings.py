aea7d3fe2796f4fef77a1ebcef7451e51108b75a
ravillaanil <anil@detrix.io>
2024-10-20 18:58:58 +0530

    removed docstrings in project

--- inventtrack/inventtrack/settings.py
+++ inventtrack/inventtrack/settings.py
@@ -75,9 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'inventtrack.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
